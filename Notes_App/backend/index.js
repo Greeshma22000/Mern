f33b2c21413b16a079840d241fc1cc4af4fd6342
Greeshma Madala <83536924+Greeshma22000@users.noreply.github.com>
2025-11-23 14:00:11 +0530

    .

--- Notes_App/backend/index.js
+++ Notes_App/backend/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 app.listen(port, () =>{
     console.log(`Server is running in ${port}✅✅....!`);
