89e5095cae685d1626cb1c569bc366862e524206
Greeshma Madala <83536924+Greeshma22000@users.noreply.github.com>
2025-11-24 13:59:30 +0530

    .

--- Notes_App/backend/index.js
+++ Notes_App/backend/index.js
@@ -1,11 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import authRoutes from "./routes/auth.js";
+import noteRoutes from "./routes/notes.js";
 
 dotenv.config();
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use("/api/users", authRoutes);
+ap.use("/api/notes", noteRoutes);
 
 connectDB();
 
--- Notes_App/backend/routes/auth.js
+++ Notes_App/backend/routes/auth.js
@@ -1,67 +1,68 @@
 import express from "express";
 import User from "../models/User.js";
-import {protect} from "../middleware/auth.js";
+import { protect } from "../middleware/auth.js";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
+// Register
 router.post("/register", async (req, res) => {
-    const {username, email, password} = req.body;
+  const { username, email, password } = req.body;
 
-    try {
-        if(!username || !email || !password) {
-            return res.status(400).json({message: "Please fill all the fields"})
-        }
-
-        const userExists = await User.findOne({email});
-
-        if(userExists){
-            return res.status(400).json({message: "User already exists"})
-        }
-
-        const user = await User.create({username, email, password});
-        const token = generateToken(user._id);
-        res.status(201).json({
-            id: user._id,
-            username: user.username,
-            email: user.email,
-            token
-        })
-    } catch (error) {
-        res.status(500).json({message: "Server error"})
+  try {
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "Please fill all the fields" });
     }
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const user = await User.create({ username, email, password });
+    const token = generateToken(user._id);
+    res.status(201).json({
+      id: user._id,
+      username: user.username,
+      email: user.email,
+      token,
+    });
+  } catch (err) {
+    console.log("Register Error: ", err);
+    res.status(500).json({ message: "Server error" });
+  }
 });
 
-router.post("/login", async(req, res) => {
-    const {email, password} = req.body;
-    try {
-        const user = await User.find({email});
-        if(!user || !(await user.matchpassword(password))){
-           return res.status(401).json({message: "Invalid credentials"});
-        }
+// Login
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
 
-        const token = generateToken(user._id);
-        res.json({
-            id: user._id,
-            username: user.username,
-            email: user.email,
-            token
-        });
-    } catch (error) {
-        res.status(500).json({message: "Server error"});
+  try {
+    const user = await User.findOne({ email });
+    if (!user || !(await user.matchPassword(password))) {
+      return res.status(401).json({ message: "Invalid credentials" });
     }
+    const token = generateToken(user._id);
+    res.json({
+      id: user._id,
+      username: user.username,
+      email: user.email,
+      token,
+    });
+  } catch (err) {
+    console.log("Login Error: ", err);
+    res.status(500).json({ message: "Server error" });
+  }
 });
 
 // Me
-router.get("/me", protect, async(req,res) => {
-    res.status(200).json(req.user)
-})
+router.get("/me", protect, async (req, res) => {
+  res.status(200).json(req.user);
+});
 
 // Generate JWT
 const generateToken = (id) => {
-    return jwt.sign(
-        {id}, 
-        process.env.JWT_SECRET, 
-        {expriesIn: "30d"}
-    );
-}
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
+};
+
 export default router;
